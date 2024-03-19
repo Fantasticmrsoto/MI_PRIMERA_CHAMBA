e803e9de36bc4a26467021689dad3b11a20f3608
fantasticmrsoto <leonelsoto2006@gmail.com>
2024-03-19 00:23:03 -0400

    estilos

---
+++ script.js
@@ -0,0 +1,34 @@
+let variable= 4
+
+if (variable % 3) {
+    console.log("se cumplio")
+} else {
+    console.log("no se cumplio")
+}
+
+
+
+const lista = [
+    "cebolla",
+    "aceite",
+    "cafe",
+    "flips",
+    "pimenton",
+    "leche",
+    "toddy",
+    "harina",
+]
+
+const usuario = {
+    name: "Leonardo",
+    age: 22,
+    password: "1234",
+    job: "Frontend develop",
+}
+
+undefined
+
+null
+
+
+console.log(usuario.jobs)
